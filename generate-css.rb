dbf7b4a3fe12b43b492606959372cdee6f9f5737
Oleg Orlov <orelcokolov@gmail.com>
2019-04-23 01:27:02 +0300

    Update

--- generate-css.rb
+++ generate-css.rb
@@ -1,19 +1,21 @@
+# coding: utf-8
+
 require "erb"
 require 'base64'
 
-font_italic64 = Base64.strict_encode64(File.read("fonts/SovietItalic.woff")).strip
-font_regular64 = Base64.strict_encode64(File.read("fonts/Soviet.woff")).strip
+font_italic64 = Base64.strict_encode64(File.open("fonts/SovietItalic.woff", "rb"){|f| f.read}).strip
+font_regular64 = Base64.strict_encode64(File.open("fonts/Soviet.woff", "rb"){|f| f.read}).strip
 
 template =  ERB.new <<-EOF
 
 @font-face {
     font-family: 'SovietBook';
-    src: url("data:application/x-font-woff;charset=utf-8;base64,<%= font_regular64 %>");
+    src: url("data:application/x-font-woff;charset=utf-8;base64,<%= font_regular64 %>")  format('woff');
 }
 
 @font-face {
     font-family: 'SovietBook';
-    src: url("data:application/x-font-woff;charset=utf-8;base64,<%= font_italic64 %>");
+    src: url("data:application/x-font-woff;charset=utf-8;base64,<%= font_italic64 %>")  format('woff');
     font-style: italic;
 }
 
